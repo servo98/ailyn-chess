8059e43f2ebce5d20ca55717ec749edbf1812429
servo pcerda <fernandonando98@live.com.mx>
2023-09-07 02:01:14 -0600

    changes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
+//Constante que contiene el tablero de chessboardjs
 const board1 = Chessboard('board1', 'start');
 
+//Constante que contiene el botón de iniciar
 const botonIniciar = document.getElementById('boton1');
+//Constante que contiene el botón de parar
 const botonParar = document.getElementById('boton2');
+
+//Constante que contiene el texto si está leyendo o está parado
+/**
+ * para saber si puedo cargar mi código en el arduino
+ * no se puede usar arduino IDE y esta página al mismo tiempo
+ */
 const estadoInput = document.getElementById('estado');
 
 let leyendo = false;
 let port;
 
+/**
+ * ancho y alto de mi matriz de botones, próximamente serán los 8x8
+ */
 const ancho = 3;
 const alto = 3;
 
-//selecting place
+//Estados válidos del juego "selecting" "place" para saber si estoy levantando la pieza o la estoy colocando
+//
 let estado = 'selecting';
 let from = '';
 let to = '';
@@ -38,9 +51,11 @@ botonIniciar.addEventListener('click', async () => {
           if (estado == 'selecting') {
             from = chessCoords;
             estado = 'place';
+            console.log('Seleccionando', chessCoords);
           } else {
             to = chessCoords;
             estado = 'selecting';
+            console.log(`Moviendo: ${from}-${to}`);
             board1.move(`${from}-${to}`);
           }
         }
@@ -67,11 +82,17 @@ function tieneCero(cadenaLimpia) {
   return cadenaLimpia.includes('0');
 }
 
+//Conversor de cadena ej: 11110111 a posiciones de matriz
 function cadenaToChessCoords(cadenaLimpia) {
+  /**
+   * obtenemos en qué posicion de la cadena de 111101
+   */
   const zeroIndex = cadenaLimpia.indexOf('0');
+  // convertimos posición del cero en coordenadas x, y
   const x = zeroIndex % ancho;
   const y = Math.floor(zeroIndex / alto);
 
+  //Convertimos esas x, y en notación de ajedrez
   const coorAjedrez = matrizACoordenadasAjedrez(y, x);
 
   console.log({
@@ -84,11 +105,13 @@ function cadenaToChessCoords(cadenaLimpia) {
   return coorAjedrez;
 }
 
+//Convierte posiciones de una matriz a coordenadas de ajedrez
 function matrizACoordenadasAjedrez(y, x) {
   const letrasFilas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
   const numerosColumnas = [8, 7, 6, 5, 4, 3, 2, 1];
   const letraFila = letrasFilas[y];
   const numeroColumna = numerosColumnas[x];
 
+  //concatena letra con número de columna
   return letraFila + numeroColumna;
 }
