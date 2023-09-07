83f3e23564b4bd172c202492914c499c70576325
servo pcerda <fernandonando98@live.com.mx>
2023-09-06 22:34:51 -0600

    add read from serial

--- js/main.js
+++ js/main.js
@@ -2,11 +2,32 @@ const board1 = Chessboard('board1', 'start');
 
 const boton = document.getElementById('boton1');
 
+async function readData(port) {
+  while (true) {
+    const reader = port.readable.getReader();
+    try {
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+          break;
+        }
+        // Procesa los datos recibidos, por ejemplo, muestra en la página web
+        console.log('Dato recibido:', value);
+      }
+    } catch (error) {
+      console.error('Error al leer datos del Arduino:', error);
+    } finally {
+      reader.releaseLock();
+    }
+  }
+}
+
 boton.addEventListener('click', async () => {
   try {
     //Ver si está conectado el arduino usb
     const port = await navigator.serial.requestPort();
     await port.open({ baudRate: 9600 });
+    readData(port);
     console.log(port);
   } catch (error) {
     console.error('ERROR en leer puerto', error);
@@ -23,23 +44,4 @@ boton.addEventListener('click', async () => {
 //   }
 // }
 
-// // Lee datos del Arduino
-// async function readData(port) {
-//   while (true) {
-//     const reader = port.readable.getReader();
-//     try {
-//       while (true) {
-//         const { value, done } = await reader.read();
-//         if (done) {
-//           break;
-//         }
-//         // Procesa los datos recibidos, por ejemplo, muestra en la página web
-//         console.log('Dato recibido:', value);
-//       }
-//     } catch (error) {
-//       console.error('Error al leer datos del Arduino:', error);
-//     } finally {
-//       reader.releaseLock();
-//     }
-//   }
-// }
+// Lee datos del Arduino
